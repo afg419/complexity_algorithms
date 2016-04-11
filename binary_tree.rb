cd5eb4260aa3d87bbd88a33c7ea06e82a4ac0733
Aaron Greenspan <afg419@gmail.com>
2016-04-10 20:27:22 -0600

    Refactors to niltree class

--- binary_tree.rb
+++ binary_tree.rb
@@ -4,62 +4,54 @@ class BinaryTree
   attr_reader :index, :data
   attr_accessor :left, :right
 
-  def initialize(opts) ##{index: 4, data: "hey"} {data: "hey", index: 4}
+  def initialize(opts)
     @index = opts[:index]
     @data = opts[:data]
-    @left = nil
-    @right = nil
+    @left = NilTree.new
+    @right = NilTree.new
   end
 
-  def add(opts) #opts = {index: 2, data: "dope"}
+  def add(opts, me = self, direction = :none)
     new_index = opts[:index]
     new_data = opts[:data]
 
     if new_index < index
-      add_to_left_side(opts)
+      left.add(opts, self, :left)
     elsif index < new_index
-      add_to_right_side(opts)
-    end
-  end
-
-  def add_to_left_side(opts)
-    if left.nil?
-      @left = create_new_tree(opts)
-    else
-      left.add(opts)
-    end
-  end
-
-  def add_to_right_side(opts)
-    if right.nil?
-      @right = create_new_tree(opts)
-    else
-      right.add(opts)
+      right.add(opts, self, :right)
     end
   end
 
   def search(new_index)
     if new_index < index
-      search_side(new_index, left)
+      left.search(new_index)
     elsif index < new_index
-      search_side(new_index, right)
+      right.search(new_index)
     else
       data
     end
   end
+end
 
-  def search_side(new_index, direction)
-    if direction.nil?
-      "DNE"
-    else
-      direction.search(new_index)
+class NilTree
+  def add(opts, me, direction)
+    if direction == :left
+      me.left = BinaryTree.new(opts)
+    elsif direction == :right
+      me.right = BinaryTree.new(opts)
     end
   end
 
-  def create_new_tree(opts)
-    BinaryTree.new(opts)
+  def search(new_index)
+    "DNE"
   end
+end
 
+class OperationCounter
+  attr_accessor :counter
+  def initialize
+    @counter = 0
+  end
 end
 
 bt = BinaryTree.new({index: 3, data: "yo"})
@@ -68,3 +60,4 @@ bt.add({index: 4, data: "d4"})
 bt.add({index: 5, data: "d5"})
 puts bt.search(5)
 puts bt.search(2)
+puts bt.search(6)
